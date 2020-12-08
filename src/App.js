5e47c8faa86410bd2eafa2a19ff894f808645147
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-12-08 19:56:18 +0100

    added basic logic to display through props and API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Pokecard from './components/Pokecard'
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Pokecard
+        id = {4}
+        name = "Charmander"
+        type = "fire"
+        base_experience = {62} 
+      />
     </div>
   );
 }
