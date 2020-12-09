34aa820d47b51604d5f37aef0718fdc4a4866eb7
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-12-09 21:46:15 +0100

    finished the exercise, basic styling

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -16,10 +16,16 @@ class Pokedex extends Component {
           ]
         }
         render() {
+          let title;
+          if(this.props.isWinner){
+            title = <h1 className="Pokedex-winner">Winning Hand!</h1>
+          }else{
+            title = <h1 className="Pokedex-loser">Losing Hand!</h1>
+          }
           return (
             <div className = "Pokedex">
-                <h1>{this.props.hand} Pokedex!</h1>
-                <h2>{this.props.exp} total experience</h2>
+                {title}
+                <p>{this.props.exp} total experience</p>
                 <div className = "Pokedex-container">
                     {this.props.pokemon.map ((p) => (
                         <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
