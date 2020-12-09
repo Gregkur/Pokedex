127fc225e178e4b136a85022780c806208df4051
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-12-09 14:28:37 +0100

    Added api and a function to chenge ids to work with the api

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react"
 import '../stylesheets/Pokecard.css'
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const NEW_POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
 
 class Pokecard extends Component {
     render() {
-        let imgSrc = `${POKE_API}${this.props.id}.png`
+        const numberPadding = () => {
+            if (this.props.id < 10){
+                return `${NEW_POKE_API}00${this.props.id}.png`
+            }else if (this.props.id < 100){ 
+                return `${NEW_POKE_API}0${this.props.id}.png` 
+            }else{
+                return `${NEW_POKE_API}${this.props.id}.png` 
+            }
+        }
+        let link = numberPadding()
         return (
             <div className = "Pokecard">
                 <h1>{this.props.name}</h1>
-                <img src={imgSrc} alt={this.props.name}/>
+                <img src={link} alt={this.props.name}/>
                 <div>type: {this.props.type}</div>
                 <div>EXP: {this.props.exp}</div>
             </div>
