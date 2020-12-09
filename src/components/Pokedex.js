6c5f8299dfe8eb829289931d511386d9a1db8a4e
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-12-09 20:28:13 +0100

    added logic to get random cards in two hands

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -3,8 +3,8 @@ import '../stylesheets/Pokedex.css'
 import Pokecard from "./Pokecard";
 
 class Pokedex extends Component {
-    static defaultProps = {
-        pokemon : [
+  static defaultProps = {
+    pokemon : [
             {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
             {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
             {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
@@ -14,9 +14,9 @@ class Pokedex extends Component {
             {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
             {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
           ]
-    }
-    render() {
-        return (
+        }
+        render() {
+          return (
             <div className = "Pokedex">
                 <h1>Pokedex!</h1>
                 <div className = "Pokedex-container">
@@ -24,7 +24,6 @@ class Pokedex extends Component {
                         <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
                     ))}
                 </div>
-
             </div>
         )
     }
---
+++ src/components/Pokegame.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react"
+import '../stylesheets/Pokegame.css'
+import Pokedex from "./Pokedex";
+
+class Pokegame extends Component {
+  static defaultProps = {
+    pokemon : [
+            {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
+            {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
+            {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
+            {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
+            {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
+            {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
+            {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
+            {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
+          ]
+        }
+    render() {
+       let firstHand = []
+       let secondHand = [ ...this.props.pokemon ]
+
+          while (firstHand.length < secondHand.length) {
+            let index = Math.floor(Math.random() * secondHand.length );
+            let randomPokemon = secondHand.splice(index, 1)[0];
+            firstHand.push(randomPokemon);
+          }
+          
+        return (
+          <div>
+            <h1>Pokegame!</h1>
+          </div>
+        )
+    }
+}
+
+export default Pokegame;
+
+        // const get_random =  (list) => {
+        //   return list[Math.floor((Math.random()*list.length))];
+        // } 
+        // console.log(get_random(this.props.pokemon))
